477b01adb62f176a32be4d1dbc887a99b1f2f2c3
Ryan Bethel <ryan.bethel@gmail.com>
2023-01-27 15:48:56 -0500

    more refactoring

--- app/pages/profiles/new.mjs
+++ app/pages/profiles/new.mjs
@@ -98,8 +98,8 @@ export default function Html ({ html, state }) {
         let arr = dataurl.split(',') 
         const mime = arr[0].match(/:(.*?);/)[1]
         const bstr = atob(arr[1])
-        let n = bstr.length
         const u8arr = new Uint8Array(n);
+        let n = bstr.length
         while(n--){
           u8arr[n] = bstr.charCodeAt(n);
         }
